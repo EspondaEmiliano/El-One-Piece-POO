5a8ff8ce551bcfbaf02ee936314bbe8c157e52ae
Ian <ianfrancomiguez@gmail.com>
2026-08-24 15:56:28 -0300

    Método calcularPromedio

--- src/GuiaDeEjercitacion/RegistroTemperaturas.java
+++ src/GuiaDeEjercitacion/RegistroTemperaturas.java
@@ -9,12 +9,22 @@ public class RegistroTemperaturas {
         temperaturaSemana = new double[7];
     }
 
-    public void registrarTemperatura(double temperatura) {
+    public void cargarTemperatura() {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < 7; i++) {
             System.out.print("Ingrese la temperatura del día " + (i + 1) + ": ");
             temperaturaSemana[i] = sc.nextDouble();
         }
+    }
+
+    public void calcularPromedio() {
+        double suma = 0;
+        double promedio;
+        for (int i = 0; i < temperaturaSemana.length; i++) {
+            suma += temperaturaSemana[i];
+        }
+        promedio = suma / temperaturaSemana.length;
+        System.out.println("El promedio de temperaturas es: " + promedio);
     }
 
 }
