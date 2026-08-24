9e75273755ab04f46fad6e512994c8412a0dc359
Ian <ianfrancomiguez@gmail.com>
2026-08-24 15:52:57 -0300

    Método cargarTemperatura

--- src/GuiaDeEjercitacion/RegistroTemperaturas.java
+++ src/GuiaDeEjercitacion/RegistroTemperaturas.java
@@ -1,4 +1,20 @@
 package GuiaDeEjercitacion;
 
+import java.util.Scanner;
+
 public class RegistroTemperaturas {
+    private double[] temperaturaSemana;
+
+    public RegistroTemperaturas() {
+        temperaturaSemana = new double[7];
+    }
+
+    public void registrarTemperatura(double temperatura) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 7; i++) {
+            System.out.print("Ingrese la temperatura del día " + (i + 1) + ": ");
+            temperaturaSemana[i] = sc.nextDouble();
+        }
+    }
+
 }
